c6f5d9fe033da943b77476e1b101862d71a4c4ec
蒋津 <jiangj01@mysoft.com.cn>
2018-10-27 12:00:05 +0800

    feat: cli param

--- src/gen.ts
+++ src/gen.ts
@@ -5,12 +5,15 @@ import * as path from 'path';
 import * as base from "./base";
 import { Options } from "./base";
 
+const configFile = path.join(process.cwd(), 'grpc-code-gen.config.js');
+
 const packageJson = require('../package.json');
 program
   .version(packageJson.version)
   .option('-b, --branch [branch]', 'Branch of proto')
   .option('-t, --token [token]', 'Access token for this repository')
   .option('-d, --dir [dir]', 'Base dir of the code gen')
+  .option('-c, --config [config]', 'Config file path', configFile)
   .option('-u, --url <items>', 'Urls of git repository, split by \',\'', function list(val) {
     return val.split(',');
   })
@@ -21,21 +24,27 @@ const {
   branch,
   token,
   dir,
+  config,
 } = program;
 
-const opt = {};
+const opt: any = {};
 
-const configFile = path.join(process.cwd(), 'grpc-code-gen.config.js');
-if (fs.existsSync(configFile)) {
-  Object.assign(opt, require(configFile));
+if (fs.existsSync(config)) {
+  Object.assign(opt, require(config));
 }
 
-assign(opt, {
-  gitUrls: url,
-  branch: branch,
-  accessToken: token,
-  baseDir: dir,
-});
+if (url) {
+  opt.gitUrls = url;
+}
+if (branch) {
+  opt.branch = branch;
+}
+if (token) {
+  opt.accessToken = token;
+}
+if (dir) {
+  opt.baseDir = dir;
+}
 
 base.gen(opt as Options)
   .then((dir: string) => {
---
+++ test/grpc-code-gen.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  gitUrls: [
+    'git@git.myscrm.cn:2c/panther-statistics-proto.git',
+    'git@git.myscrm.cn:2c/panther-third-proto.git',
+  ],
+  branch: 'test',
+  accessToken: '${token}',
+  baseDir: `${__dirname}/code-gen`,
+};
